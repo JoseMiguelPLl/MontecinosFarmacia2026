6f8beee1035635c4de4576d7a799f71e99194451
devstudiobol <devstudiobol@gmail.com>
2026-04-08 10:06:13 -0400

    Actualización: describir lo que hiciste

--- src/components/ProductManagement.tsx
+++ src/components/ProductManagement.tsx
@@ -19,7 +19,7 @@ interface Product {
   precio: number;
   stock: number;
   concentracion: number;
-  casilla: string;
+  casilla: number;
   vencimiento: string;
   idpresentacion: number;
   idlaboratorio: number;
@@ -61,7 +61,7 @@ const initialProductState: Partial<Product> = {
   precio: 0,
   stock: 0,
   concentracion: 0,
-  casilla: '',
+  casilla: 0,
   vencimiento: '',
   idpresentacion: 0,
   idlaboratorio: 0,
@@ -202,13 +202,13 @@ const ProductManagement: React.FC = () => {
     try {
       const params = new URLSearchParams();
       params.append('id', editProducto.id.toString());
-      params.append('codigo', formData.codigo || 'S/D');
+      params.append('codigo', formData.codigo || '');
       params.append('nombre', formData.nombre || '');
       params.append('descripcion', formData.descripcion || '');
       params.append('precio', formData.precio?.toString() || '0');
       params.append('stock', formData.stock?.toString() || '0');
       params.append('concentracion', formData.concentracion?.toString() || '0');
-      params.append('casilla', formData.casilla || 'S/D');
+      params.append('casilla', formData.casilla?.toString() || '0');
       params.append('vencimiento', formData.vencimiento || '');
       params.append('idpresentacion', formData.idpresentacion?.toString() || '0');
       params.append('idlaboratorio', formData.idlaboratorio?.toString() || '0');
@@ -279,7 +279,7 @@ const ProductManagement: React.FC = () => {
         </div>
         <Card>
           <CardHeader>
-            <CardTitle>Gestión de Productoss</CardTitle>
+            <CardTitle>Gestión de Productos</CardTitle>
           </CardHeader>
           <CardContent>
             {alert.show && (
@@ -471,7 +471,7 @@ const ProductManagement: React.FC = () => {
                   <input  
                     type="text"
                     name="casilla"
-                    value={formData.casilla || 'S/D'}
+                    value={formData.casilla || 0}
                     onChange={handleInputChange}
                     className="form-input2"
                     placeholder="N"
