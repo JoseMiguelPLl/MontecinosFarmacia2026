cd2be1ae8139ac2bdf6589f7c7952d913ec78ddd
devstudiobol <devstudiobol@gmail.com>
2026-06-08 08:49:17 -0400

    Actualización del Dashboard y paginación de la tabla

--- src/components/Ventas.tsx
+++ src/components/Ventas.tsx
@@ -72,7 +72,7 @@ interface Producto {
   idlaboratorio: number;
   idtipo: number;
  nombre_generico: string;
- Vecimiento: string;
+ Vencimiento: string;
 }
 
 interface Tipo {
@@ -958,7 +958,7 @@ const filteredProducto = searchProducto
                           </td>
                           <td className="px-2 py-1.5 text-center whitespace-nowrap">
       {(() => {
-        const infoVence = obtenerDiasRestantes(producto.Vecimiento);
+        const infoVence = obtenerDiasRestantes(producto.Vencimiento);
         return <span className={infoVence.clase}>{infoVence.texto}</span>;
       })()}
     </td>
